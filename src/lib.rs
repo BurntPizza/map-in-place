6e556e2dc8ee2898457c863c207f8cd26ed78eb8
BurntPizza <xburntpizza@gmail.com>
2016-05-30 16:37:44 -0400

    Refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
 
+#[cfg(test)]
+#[macro_use]
+extern crate lazy_static;
+
+use std::ptr;
+use std::mem;
+use std::cmp::{Ord, Ordering};
+
 pub trait MapInPlace<A, B>: Sized {
     /// Should be of the same base type as the implementor.  
     /// E.g. `Vec<B>` when implementing for `Vec<A>`
@@ -13,6 +21,17 @@ pub trait MapInPlace<A, B>: Sized {
     fn map_in_place<F>(self, f: F) -> Self::Output where F: FnMut(A) -> B;
 }
 
+unsafe fn map_in_place<A, B, F>(ptr_a: *const A, ptr_b: *mut B, len: usize, mut f: F)
+    where F: FnMut(A) -> B
+{
+    for i in 0..len {
+        let ptr_a = ptr_a.offset(i as isize);
+        let ptr_b = ptr_b.offset(i as isize);
+
+        ptr::write(ptr_b, f(ptr::read(ptr_a)));
+    }
+}
+
 impl<A, B> MapInPlace<A, B> for Vec<A> {
     type Output = Vec<B>;
 
@@ -20,29 +39,6 @@ impl<A, B> MapInPlace<A, B> for Vec<A> {
     fn map_in_place<F>(mut self, mut f: F) -> Self::Output
         where F: FnMut(A) -> B
     {
-        use std::mem;
-        use std::ptr;
-        use std::cmp::{Ord, Ordering};
-
-        macro_rules! map_loop {
-            () => {{
-                let ptr_a = self.as_ptr();
-                let ptr_b = ptr_a as *mut B;
-                let len = self.len();
-
-                for i in 0..len {
-                    unsafe {
-                        let ptr_a = ptr_a.offset(i as isize);
-                        let ptr_b = ptr_b.offset(i as isize);
-
-                        ptr::write(ptr_b, f(ptr::read(ptr_a)));
-                    }
-                }
-
-                (ptr_b, len)
-            }}
-        }
-
         let a_size = mem::size_of::<A>();
         let b_size = mem::size_of::<B>();
 
@@ -65,14 +61,20 @@ impl<A, B> MapInPlace<A, B> for Vec<A> {
                     v
                 } else {
                     let cap = self.capacity().checked_mul(a_size).map(|x| x / b_size).unwrap();
-                    let (ptr_b, len) = map_loop!();
+                    let ptr_a = self.as_ptr();
+                    let ptr_b = ptr_a as *mut B;
+                    let len = self.len();
 
-                    mem::forget(self);
-
-                    unsafe { Vec::from_raw_parts(ptr_b, len, cap) }
+                    unsafe {
+                        map_in_place(ptr_a, ptr_b, len, f);
+                        mem::forget(self);
+                        Vec::from_raw_parts(ptr_b, len, cap)
+                    }
                 }
             }
-            Ordering::Less => panic!(),
+            Ordering::Less => {
+                panic!("map_in_place(Vec<A>): Size of A must be greater than or equal to size of B")
+            }
         }
     }
 }
@@ -81,13 +83,9 @@ impl<'a, A, B: 'a> MapInPlace<A, B> for &'a mut [A] {
     type Output = &'a mut [B];
 
     #[inline]
-    fn map_in_place<F>(self, mut f: F) -> Self::Output
+    fn map_in_place<F>(self, f: F) -> Self::Output
         where F: FnMut(A) -> B
     {
-        use std::mem;
-        use std::ptr;
-        use std::cmp::{Ord, Ordering};
-
         let a_size = mem::size_of::<A>();
         let b_size = mem::size_of::<B>();
 
@@ -96,19 +94,12 @@ impl<'a, A, B: 'a> MapInPlace<A, B> for &'a mut [A] {
                 let ptr_a = self.as_ptr();
                 let ptr_b = ptr_a as *mut B;
 
-                for i in 0..self.len() {
-                    unsafe {
-                        let ptr_a = ptr_a.offset(i as isize);
-                        let ptr_b = ptr_b.offset(i as isize);
-
-                        ptr::write(ptr_b, f(ptr::read(ptr_a)));
-                    }
+                unsafe {
+                    map_in_place(ptr_a, ptr_b, self.len(), f);
+                    mem::transmute(self)
                 }
-
-                unsafe { mem::transmute(self) }
             }
-            Ordering::Greater => panic!(),
-            Ordering::Less => panic!(),
+            _ => panic!("map_in_place(&mut [A]): Size of A must be equal to size of B"),
         }
     }
 }
