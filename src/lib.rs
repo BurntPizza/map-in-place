e7be9b7372ff875e21a83e29d875d2ec35550aaa
BurntPizza <xburntpizza@gmail.com>
2016-05-30 21:15:17 -0400

    Working on panic safety

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #[macro_use]
 extern crate lazy_static;
 
+use std::marker::PhantomData;
 use std::ptr;
 use std::mem;
 use std::cmp::{Ord, Ordering};
@@ -21,15 +22,66 @@ pub trait MapInPlace<A, B>: Sized {
     fn map_in_place<F>(self, f: F) -> Self::Output where F: FnMut(A) -> B;
 }
 
+struct Dropper<A, B> {
+    idx: usize,
+    owned: Vec<A>,
+    _marker: PhantomData<B>,
+}
+
+impl<A, B> Drop for Dropper<A, B> {
+    #[inline]
+    fn drop(&mut self) {
+        let owned = &mut self.owned;
+        let idx = self.idx;
+        let len = owned.len();
+        let ptr_a = owned.as_mut_ptr();
+        let ptr_b = ptr_a as *mut B;
+
+        unsafe {
+            owned.set_len(0);
+
+            if idx != len {
+                // panicked; manual cleanup needed
+                for i in 0..idx {
+                    ptr::drop_in_place(ptr_b.offset(i as isize));
+                }
+
+                for i in idx..len {
+                    ptr::drop_in_place(ptr_a.offset(i as isize));
+                }
+            } else {
+                // everything went well, no cleanup required
+
+                let mut tmp = Vec::with_capacity(0);
+                mem::swap(&mut tmp, owned);
+                mem::forget(tmp);
+            }
+        }
+    }
+}
+
 #[inline]
-unsafe fn map_in_place<A, B, F>(ptr_a: *const A, ptr_b: *mut B, len: usize, mut f: F)
+unsafe fn map_in_place<A, B, F>(owned: Vec<A>, mut f: F)
     where F: FnMut(A) -> B
 {
+    let ptr_a = owned.as_ptr();
+    let ptr_b = ptr_a as *mut B;
+    let len = owned.len();
+
+    let mut dropper = Dropper {
+        idx: 0,
+        owned: owned,
+        _marker: PhantomData::<B>,
+    };
+
     for i in 0..len {
         let ptr_a = ptr_a.offset(i as isize);
         let ptr_b = ptr_b.offset(i as isize);
 
-        ptr::write(ptr_b, f(ptr::read(ptr_a)));
+        let v = ptr::read(ptr_a);
+        dropper.idx += 1;
+
+        ptr::write(ptr_b, f(v));
     }
 }
 
@@ -37,17 +89,31 @@ impl<A, B> MapInPlace<A, B> for Vec<A> {
     type Output = Vec<B>;
 
     #[inline]
-    fn map_in_place<F>(mut self, mut f: F) -> Self::Output
+    fn map_in_place<F>(self, mut f: F) -> Self::Output
         where F: FnMut(A) -> B
     {
         let a_size = mem::size_of::<A>();
         let b_size = mem::size_of::<B>();
+        let ptr_a = self.as_ptr();
+        let ptr_b = ptr_a as *mut B;
+        let len = self.len();
 
         match a_size.cmp(&b_size) {
             Ordering::Equal => {
-                self.as_mut_slice().map_in_place(f);
+                let cap = self.capacity();
 
-                unsafe { mem::transmute(self) }
+                unsafe {
+                    if a_size == 0 {
+                        for _ in 0..len {
+                            f(ptr::read(ptr_a));
+                        }
+
+                        mem::transmute(self)
+                    } else {
+                        map_in_place(self, f);
+                        Vec::from_raw_parts(ptr_b, len, cap)
+                    }
+                }
             }
             Ordering::Greater => {
                 if b_size == 0 {
@@ -69,13 +135,9 @@ impl<A, B> MapInPlace<A, B> for Vec<A> {
                         assert_eq!(tmp % b_size, 0);
                         tmp / b_size
                     };
-                    let ptr_a = self.as_ptr();
-                    let ptr_b = ptr_a as *mut B;
-                    let len = self.len();
 
                     unsafe {
-                        mem::forget(self);
-                        map_in_place(ptr_a, ptr_b, len, f);
+                        map_in_place(self, f);
                         Vec::from_raw_parts(ptr_b, len, cap)
                     }
                 }
@@ -87,36 +149,8 @@ impl<A, B> MapInPlace<A, B> for Vec<A> {
     }
 }
 
-impl<'a, A, B: 'a> MapInPlace<A, B> for &'a mut [A] {
-    type Output = &'a mut [B];
-
-    #[inline]
-    fn map_in_place<F>(self, f: F) -> Self::Output
-        where F: FnMut(A) -> B
-    {
-        let a_size = mem::size_of::<A>();
-        let b_size = mem::size_of::<B>();
-
-        match a_size.cmp(&b_size) {
-            Ordering::Equal => {
-                let ptr_a = self.as_ptr();
-                let ptr_b = ptr_a as *mut B;
-
-                unsafe {
-                    map_in_place(ptr_a, ptr_b, self.len(), f);
-                    mem::transmute(self)
-                }
-            }
-            _ => panic!("map_in_place(&mut [A]): Size of A must be equal to size of B"),
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
-
-
-
     use super::MapInPlace;
 
     use std::mem;
@@ -146,9 +180,9 @@ mod tests {
             }
         }
 
-        assert_eq!(mem::size_of::<X>(), mem::size_of::<Y>()); // will use slice impl
+    assert_eq!(mem::size_of::<X>(), mem::size_of::<Y>()); // will use slice impl
 
-        let v = vec![X(0), X(1), X(2), X(3)];
+    let v = vec![X(0), X(1), X(2), X(3)];
 
         let bp = v.as_ptr() as *const ();
         let v = v.map_in_place(|X(v)| Y(v));
@@ -164,7 +198,7 @@ mod tests {
         assert_eq!(bp, ap); // still at same memory addr
         assert_eq!(v, expected);
 
-        mem::drop(v);
+    mem::drop(v);
 
         {
             let drops = DROPS.lock().unwrap().clone();
@@ -204,7 +238,7 @@ mod tests {
         #[derive(Debug)]
         struct Zst;
 
-        let v = vec![(), (), (), ()];
+    let v = vec![(), (), (), ()];
 
         let bp = v.as_ptr() as *const ();
         let v = v.map_in_place(|_| Zst);
@@ -231,71 +265,6 @@ mod tests {
 
         let bp = v.as_ptr() as *const ();
         let v = v.map_in_place(|_| 0usize);
-        let ap = v.as_ptr() as *const ();
-
-        assert_eq!(bp, ap); // still at same memory addr
-    }
-
-    // //////
-
-    #[test]
-    fn same_size_slice() {
-        let mut v = vec![0, 1, 2, 3];
-
-        let bp = v.as_ptr() as *const ();
-        let v = (&mut *v).map_in_place(|x: u32| (x * x) as i32);
-        let ap = v.as_ptr() as *const ();
-
-        assert_eq!(bp, ap); // still at same memory addr
-        assert_eq!(v, &*vec![0, 1, 4, 9]);
-    }
-
-    #[test]
-    #[should_panic]
-    fn different_sizes_slice() {
-        let mut v = vec![0, 1, 2, 3];
-
-        let bp = v.as_ptr() as *const ();
-        let v = (&mut *v).map_in_place(|x: u32| (x * x) as i16);
-        let ap = v.as_ptr() as *const ();
-
-        assert_eq!(bp, ap); // still at same memory addr
-        assert_eq!(v, &*vec![0, 1, 4, 9]);
-    }
-
-    #[test]
-    fn both_zst_slice() {
-        #[derive(Debug)]
-        struct Zst;
-
-        let mut v = vec![(), (), (), ()];
-
-        let bp = v.as_ptr() as *const ();
-        let v = (&mut *v).map_in_place(|_| Zst);
-        let ap = v.as_ptr() as *const ();
-
-        assert_eq!(bp, ap); // still at same memory addr
-    }
-
-    #[test]
-    #[should_panic]
-    fn nzst_to_zst_slice() {
-        let mut v = vec![0, 1, 2, 3];
-
-        let bp = v.as_ptr() as *const ();
-        let v = (&mut *v).map_in_place(|_: u32| ());
-        let ap = v.as_ptr() as *const ();
-
-        assert!(bp != ap); // -- NOT -- still at same memory addr
-    }
-
-    #[test]
-    #[should_panic]
-    fn zst_to_nzst_slice() {
-        let mut v = vec![(), (), (), ()];
-
-        let bp = v.as_ptr() as *const ();
-        let v = (&mut *v).map_in_place(|_| 0usize);
         let ap = v.as_ptr() as *const ();
 
         assert_eq!(bp, ap); // still at same memory addr
